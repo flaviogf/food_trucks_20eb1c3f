a8a0934746c083ccf4b3b4f324308a3d35d5f5c7
flaviogf <flavio.fernandes6@gmail.com>
2021-07-10 14:39:41 -0300

    feat: add method to map row to FoodTruck

--- app/services/food_trucks_http_service.rb
+++ app/services/food_trucks_http_service.rb
@@ -6,8 +6,20 @@ class FoodTrucksHttpService
   FILE_URI = URI('https://data.sfgov.org/api/views/rqzj-sfat/rows.csv?accessType=DOWNLOAD')
 
   def each_slice(batch, &block)
-    Net::HTTP.get_response(FILE_URI) do |resp|
-      CSV.new(resp.body, headers: true).each_slice(batch, &block)
+    fetch_data do |data|
+      CSV.new(data, headers: true).each_slice(batch) do |rows|
+        block.call(rows.collect(&method(:food_truck_from)))
+      end
     end
+  end
+
+  private
+
+  def fetch_data(&block)
+    Net::HTTP.get_response(FILE_URI) { |resp| resp.read_body(&block) }
+  end
+
+  def food_truck_from(row)
+    FoodTruck.new(food_items: row['FoodItems'], longitude: row['Longitude'], latitude: row['Latitude'])
   end
 end

99fc868547f59f6c36b4c3233b3233facf02a46d
flaviogf <flavio.fernandes6@gmail.com>
2021-07-11 15:48:20 -0300

    feat: add method delete_all!

--- app/services/food_trucks_elasticsearch_repository.rb
+++ app/services/food_trucks_elasticsearch_repository.rb
@@ -5,8 +5,22 @@ class FoodTrucksElasticsearchRepository
     @client = client
   end
 
-  def bulk(food_trucks)
+  def insert_all(food_trucks)
     body = food_trucks.collect { |food_truck| { index: { _index: 'food-trucks-v1', data: food_truck.to_h } } }
+
+    @client.bulk(body: body)
+  end
+
+  def delete_all!
+    count = @client.count(index: 'food-trucks-v1')['count']
+
+    return unless count.positive?
+
+    hits = @client.search(index: 'food-trucks-v1', size: count).dig('hits', 'hits')
+
+    food_truck_ids = hits.collect { |hit| hit['_id'] }
+
+    body = food_truck_ids.collect { |id| { delete: { _index: 'food-trucks-v1', _id: id } } }
 
     @client.bulk(body: body)
   end
--- spec/services/food_trucks_elasticsearch_repository_spec.rb
+++ spec/services/food_trucks_elasticsearch_repository_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe FoodTrucksElasticsearchRepository do
-  describe '#bulk' do
+  describe '#insert_all' do
     let(:client) do
       double('client')
     end
@@ -12,7 +12,7 @@ RSpec.describe FoodTrucksElasticsearchRepository do
       (0..5).collect { build(:food_truck) }
     end
 
-    let(:food_truck_hashs) do
+    let(:food_truck_hashes) do
       food_trucks.collect { |food_truck| { index: { _index: 'food-trucks-v1', data: food_truck.to_h } } }
     end
 
@@ -25,9 +25,59 @@ RSpec.describe FoodTrucksElasticsearchRepository do
     end
 
     after do
-      food_trucks_elasticsearch_repository.bulk(food_trucks)
+      food_trucks_elasticsearch_repository.insert_all(food_trucks)
     end
 
-    it { expect(client).to receive(:bulk).with(body: food_truck_hashs).once }
+    it { expect(client).to receive(:bulk).with(body: food_truck_hashes).once }
+  end
+
+  describe '#delete_all!' do
+    let(:client) do
+      double('client')
+    end
+
+    context 'when count is positive' do
+      let(:hits) do
+        (0..5).collect { |id| { '_id' => id } }
+      end
+
+      let(:food_truck_hashes) do
+        (0..5).collect { |id| { delete: { _index: 'food-trucks-v1', _id: id } } }
+      end
+
+      subject(:food_trucks_elasticsearch_repository) do
+        FoodTrucksElasticsearchRepository.new(client: client)
+      end
+
+      before do
+        allow(client).to receive(:count).and_return({ 'count' => hits.count })
+        allow(client).to receive(:search).and_return({ 'hits' => { 'hits' => hits } })
+        allow(client).to receive(:bulk)
+      end
+
+      after do
+        food_trucks_elasticsearch_repository.delete_all!
+      end
+
+      it { expect(client).to receive(:bulk).with(body: food_truck_hashes).once }
+    end
+
+    context 'when count is not positive' do
+      subject(:food_trucks_elasticsearch_repository) do
+        FoodTrucksElasticsearchRepository.new(client: client)
+      end
+
+      before do
+        allow(client).to receive(:count).and_return({ 'count' => 0 })
+        allow(client).to receive(:search).and_return({ 'hits' => { 'hits' => [] } })
+        allow(client).to receive(:bulk)
+      end
+
+      after do
+        food_trucks_elasticsearch_repository.delete_all!
+      end
+
+      it { expect(client).to_not receive(:bulk) }
+    end
   end
 end

0f712bfbc458404f4ea75b0ba4c9738bf0527c92
flaviogf <flavio.fernandes6@gmail.com>
2021-07-11 15:47:23 -0300

    fix: sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,10 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://redis:6379/0' }
+
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file('config/schedule.yml')
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://redis:6379/0' }
-end
-
-Rails.application.config.after_initialize do
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file('config/schedule.yml')
 end

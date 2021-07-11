f699bf5547f0e439eca555ae37cad6a54c149c8f
flaviogf <flavio.fernandes6@gmail.com>
2021-07-11 13:38:02 -0300

    feat: configure sidekiq-cron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,3 +7,7 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://redis:6379/0' }
 end
+
+Rails.application.config.after_initialize do
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file('config/schedule.yml')
+end

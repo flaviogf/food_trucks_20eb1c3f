0a9c12cc7df62627db623090f7e8094bc4981b98
flaviogf <flavio.fernandes6@gmail.com>
2021-07-18 09:40:53 -0300

    feat: add leaflet initial configuration

--- app/javascript/packs/maps.jsx
+++ app/javascript/packs/maps.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const accessToken = 'pk.eyJ1IjoiZmxhdmlvZ2YiLCJhIjoiY2tyOTVjM3JxMHdmNzJvbmk4YW9qZTk3OCJ9.z92E7oB6wMn2YQGLoukDbw'
+
+const position = [37.7749, -122.4194]
 
 function Maps() {
   return (
-    <div>
-      <form>
-        <div>
-          <label htmlFor="latitude">Latitude</label>
-          <input type="number" id="latitude" name="latitude" />
-        </div>
-
-        <div>
-          <label htmlFor="longitude">Longitude</label>
-          <input type="number" id="longitude" name="longitude" />
-        </div>
-
-        <div>
-          <button>Search</button>
-        </div>
-      </form>
+    <div class="maps">
+      <MapContainer center={position} zoom={13} scrollWhenZoom={false} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=${accessToken}`}
+        />
+        <Marker position={position}>
+          <Popup>Center of San Francisco</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }

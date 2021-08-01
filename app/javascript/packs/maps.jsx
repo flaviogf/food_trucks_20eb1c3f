0150d5bdb6465830c675d5bbcef6b331d1b52e37
flaviogf <flavio.fernandes6@gmail.com>
2021-08-01 10:26:10 -0300

    feat: add click event listener

--- app/javascript/packs/maps.jsx
+++ app/javascript/packs/maps.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, Circle, useMapEvents } from 'react-leaflet'
 import axios from 'axios'
 
 const accessToken = 'pk.eyJ1IjoiZmxhdmlvZ2YiLCJhIjoiY2tyOTVjM3JxMHdmNzJvbmk4YW9qZTk3OCJ9.z92E7oB6wMn2YQGLoukDbw'
@@ -11,6 +11,10 @@ function Maps() {
   const [longitude, setLongitude] = useState(initialPosition[1])
   const [latitude, setLatitude] = useState(initialPosition[0])
   const [foodTrucks, setFoodTrucks] = useState([])
+
+  useEffect(() => {
+    fetchFoodTrucks({ longitude, latitude })
+  }, [])
 
   function onSubmit(e) {
     e.preventDefault()
@@ -32,14 +36,18 @@ function Maps() {
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=${accessToken}`}
         />
 
-        <Circle center={[latitude, longitude]} radius={2000} />
+        <LocationCircle
+          latitude={latitude}
+          longitude={longitude}
+          setLatitude={setLatitude}
+          setLongitude={setLongitude}
+        />
 
         {foodTrucks.map((it, index) => (
           <Marker key={String(index)} position={[it.latitude, it.longitude]}>
             <Popup>{it.food_items}</Popup>
           </Marker>
         ))}
-
       </MapContainer>
 
       <form className="maps__form" onSubmit={onSubmit}>
@@ -57,6 +65,17 @@ function Maps() {
       </form>
     </div>
   )
+}
+
+function LocationCircle({ latitude, longitude, setLatitude, setLongitude }) {
+  const map = useMapEvents({
+    click(e) {
+      setLongitude(e.latlng.lng)
+      setLatitude(e.latlng.lat)
+    },
+  })
+
+  return <Circle center={[latitude, longitude]} radius={2000} />
 }
 
 document.addEventListener('DOMContentLoaded', () => {
